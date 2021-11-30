bb873cc5d5ebe7fea26c29763b94e6d942dc471c
PaolinoRossi <a.cecchi8@studenti.unipi.it>
2021-11-30 16:34:46 +0100

    message controller tests enabled

--- swagger_server/test/test_message_controller.py
+++ swagger_server/test/test_message_controller.py
@@ -13,32 +13,50 @@ from datetime import datetime
 
 class TestMessageController(BaseTestCase):
     """MessageController integration test stubs"""
-    '''
+
+    def send_test_message(self):
+        """Util function used in tests
+
+        Sends a message and returns its id
+        """
+        body = MessagePost()
+        body.id_sender = 0
+        body.recipients_list = [1]
+        body.date_delivery = datetime.now().isoformat()
+        body.text = "test"
+        response = self.client.open(
+            '/messages',
+            method='POST',
+            data=json.dumps(body.to_dict()),
+            content_type='application/json')
+        assert response.status_code == 200
+        return int(response.json["id_message"])
+    
+    
     def test_mib_resources_message_delete_message(self):
         """Test case for mib_resources_message_delete_message
 
-        
+        Send and delete a message
         """
+        id=self.send_test_message()
         response = self.client.open(
-            '/messages/{message_id}'.format(message_id=1),
+            '/messages/{message_id}'.format(message_id=id),
             method='DELETE')
-        self.assert200(response,
-                       'Response body is : ' + response.data.decode('utf-8'))
-
+        assert response.status_code == 202
+    
     def test_mib_resources_message_get_all_messages(self):
         """Test case for mib_resources_message_get_all_messages
 
         
         """
-        query_string = [('type', 'type_example')]
+        query_string = [('type', 'sent')]
         response = self.client.open(
             '/messages',
             method='GET',
             query_string=query_string)
-        self.assert200(response,
-                       'Response body is : ' + response.data.decode('utf-8'))
-
-    def test_mib_resources_message_get_message(self):
+        assert response.status_code == 200
+    
+    def test_mib_resources_message_get_message_404(self):
         """Test case for mib_resources_message_get_message
 
         
@@ -46,9 +64,8 @@ class TestMessageController(BaseTestCase):
         response = self.client.open(
             '/messages/{message_id}'.format(message_id=1),
             method='GET')
-        self.assert200(response,
-                       'Response body is : ' + response.data.decode('utf-8'))
-    '''
+        assert response.status_code == 404
+    
     def test_mib_resources_message_send_message(self):
         """Test case for mib_resources_message_send_message
 
@@ -64,16 +81,27 @@ class TestMessageController(BaseTestCase):
             method='POST',
             data=json.dumps(body.to_dict()),
             content_type='application/json')
+        assert response.status_code == 201
+    
+    def test_mib_resources_message_get_message(self):
+        """Test case for mib_resources_message_get_message
+
+        Send and retrieve a message
+        """
+        id=self.send_test_message()
+        response = self.client.open(
+            '/messages/{message_id}'.format(message_id=id),
+            method='GET')
         assert response.status_code == 200
-    '''
+    
     def test_mib_resources_message_withdraw_message(self):
         """Test case for mib_resources_message_withdraw_message
 
-        
+        Send and withdraw a message
         """
+        id=self.send_test_message()
         response = self.client.open(
-            '/messages/{message_id}/withdraw'.format(message_id=1),
+            '/messages/{message_id}/withdraw'.format(message_id=id),
             method='POST')
-        self.assert200(response,
-                       'Response body is : ' + response.data.decode('utf-8'))
-    '''
+        assert response.status_code == 200
+    
