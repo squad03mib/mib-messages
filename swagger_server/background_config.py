575034df317c94207972f77fa7db7a432e0510fe
gystemd <gystemd@gmail.com>
2021-12-08 15:41:23 +0100

    update background_config.py

--- swagger_server/background_config.py
+++ swagger_server/background_config.py
@@ -2,5 +2,7 @@ import os
 
 timezone = 'UTC'
 
-broker_url_default = 'redis://127.0.0.1:6379'
+broker_url_default = 'redis://127.0.0.1:6389'
 broker_url = os.getenv('BROKER_URL', broker_url_default)
+result_backend_default = broker_url
+result_backend = os.getenv('RESULT_BACKEND', result_backend_default)
